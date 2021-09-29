15b35d23e1494fed74f682562c8ec830d90ed739
Volkov <formobileprima@gmail.com>
2021-09-29 23:30:45 +0300

    add increase/dercrease quantity by click in music shop

--- Musicshop/app/src/main/java/com/example/musicshop/MainActivity.java
+++ Musicshop/app/src/main/java/com/example/musicshop/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void increaseQuantity(View view) {
+        TextView quantityText = findViewById(R.id.quantity);
+        quantity += 1;
+        quantityText.setText(String.valueOf(quantity));
+    }
+
+    public void decreaseQuantity(View view) {
+        TextView quantityText = findViewById(R.id.quantity);
+        if (quantity - 1 >= 0) {
+            quantity -= 1;
+        }
+        quantityText.setText(String.valueOf(quantity));
     }
 }
