67cbe599bafb64bcf57c2fe603847ea05740211b
nizkiyVorobey <formobileprima@gmail.com>
2022-02-08 23:39:55 +0200

    change project structure

--- MyFavoriteMoviesMVVM/app/src/main/java/com/example/myfavoritemoviesmvvm/model/add_edit/AddEditViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myfavoritemoviesmvvm.model.add_edit
-
-import androidx.lifecycle.ViewModel
-import com.example.myfavoritemoviesmvvm.model.movie.Movie
-
-class AddEditViewModel : ViewModel() {
-    var behavior = "create"
-    var movie = Movie()
-}
